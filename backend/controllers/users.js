2f6ee49fb46a046a67dc6ae5104740aaeb9e0e5e
Marcelo Manchester <celoqueiroga@gmail.com>
2020-03-21 00:49:29 -0300

    init server

---
+++ backend/controllers/users.js
@@ -0,0 +1,24 @@
+const userUtils = require('../src/users.js');
+
+const getUser = (req, res) => {
+    
+}
+
+const postUser = (req, res) => {
+    
+}
+
+const deleteUser = (req, res) => {
+    
+}
+
+const patchUser = (req, res) => {
+    
+}
+
+module.exports = {
+    getUser,
+    postUser,
+    deleteUser,
+    patchUser
+}
