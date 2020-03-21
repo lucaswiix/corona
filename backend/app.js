653e1163b3a47fa6696a7b97b9f7542955874ca4
Lucas Assad <=>
2020-03-21 18:39:48 -0300

    configurando nodemon

--- backend/app.js
+++ backend/app.js
@@ -1,2 +1,25 @@
-const server = require('./index');
-require('./routes')(server);
+const http = require('http');
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const server = app.listen(port, () => {
+    console.log('running on port '+ port);
+});
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
+
+app.get('/', (req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World');
+});
+
+module.exports = app;
