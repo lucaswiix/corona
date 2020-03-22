9da8bc12be9bad51842170da6dffac7c19e50140
lucaswiix <lucasrocha.wiix@gmail.com>
2020-03-21 23:22:40 -0300

    rename

--- backend-ts/src/quarentena.ts
+++ backend-ts/src/quarentena.ts
@@ -1,4 +1,4 @@
-import thor from 'commander';
+import quarentena from 'commander';
 import * as fs from 'fs';
 import * as path from 'path';
 
@@ -15,17 +15,17 @@ const envConfigFilePath = path.resolve(
   require('dotenv').config({ path: envConfigFilePath });
 
   const app = () => require('./app').default();
-  thor.version('v0.1.0');
+  quarentena.version('v0.1.0');
 
   // Main command, Web Application
-  thor.command('app').action(app);
+  quarentena.command('app').action(app);
 
-  thor.on('command:*', () => {
+  quarentena.on('command:*', () => {
     console.error('Invalid command');
     process.exit(1);
   });
 
-  thor.parse(process.argv);
+  quarentena.parse(process.argv);
 })();
 
 async function checkEncryptedConfigFiles() {
